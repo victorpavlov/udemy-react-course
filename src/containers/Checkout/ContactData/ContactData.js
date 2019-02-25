a101a7ffa7c944636e7538262383903b32248e1b
Victor Pavlov <vitekpavlov@gmail.com>
2019-02-25 17:08:05 +0200

    Lecture 366 passed

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -92,7 +92,8 @@ class ContactData extends React.Component {
     const order = {
       ingredients: this.props.ingredients,
       totalPrice: this.props.price,
-      orderData: formData
+      orderData: formData,
+      userId: this.props.userId
     };
     this.props.onOrderBurger(order, this.props.token);
   }
@@ -193,7 +194,8 @@ const mapStateToProps = state => {
     ingredients: state.burgerBuilder.ingredients,
     price: state.burgerBuilder.totalPrice,
     loading: state.order.loading,
-    token: state.auth.token
+    token: state.auth.token,
+    userId: state.auth.userId
   };
 };
 
