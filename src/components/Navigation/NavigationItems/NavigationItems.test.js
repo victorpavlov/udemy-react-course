5ac91e11323fb4355cdec03f3bb3b97b906fd558
Victor Pavlov <vitekpavlov@gmail.com>
2019-02-27 16:19:13 +0200

    Lecture 386 passed

---
+++ src/components/Navigation/NavigationItems/NavigationItems.test.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+import { configure, shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+import NavigationItems from './NavigationItems';
+import NavigationItem from './NavigationItem/NavigationItem';
+
+configure({adapter: new Adapter()});
+
+describe('<NavigationItems />', () => {
+  let wrapper;
+  
+  beforeEach(() => {
+    wrapper = shallow(<NavigationItems />);
+  });
+
+  it('should render 2 <NavigationItem /> elements if not logged in', () => {
+    expect(wrapper.find(NavigationItem)).toHaveLength(2);
+  });
+  
+  it('should render 3 <NavigationItem /> elements if logged in', () => {
+    wrapper.setProps({
+      isAuthenticated: true
+    });
+    expect(wrapper.find(NavigationItem)).toHaveLength(3);
+  });
+
+  it('should contains <NavigationItem link="/logout">Logout</NavigationItem> element if logged in', () => {
+    wrapper.setProps({
+      isAuthenticated: true
+    });
+    expect(wrapper.contains(<NavigationItem link="/logout">Logout</NavigationItem>)).toEqual(true);
+  });
+});
